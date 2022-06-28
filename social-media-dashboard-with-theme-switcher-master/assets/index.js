0d1726997f4e5f863246aab449fb1d7186f44bf0
professorm_mr <professormr20@gmail.com>
2022-06-28 16:35:57 +0430

    add all files social Media Dashboard project

---
+++ social-media-dashboard-with-theme-switcher-master/assets/index.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const body = document.querySelector('body');
+
+toggleBtn.addEventListener('click', () =>{
+    toggleBtn.classList.toggle('active-btn');
+    body.classList.toggle('theme-dark');
+});
