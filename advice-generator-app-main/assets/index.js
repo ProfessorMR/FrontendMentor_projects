3cd249056844b6bc560e48a9f458c84904247449
professorm_mr <professormr20@gmail.com>
2022-06-28 19:45:38 +0430

    add all files Advice generator app project

---
+++ advice-generator-app-main/assets/index.js
@@ -0,0 +1,28 @@
+const dice = document.querySelector('.dice');
+const card = document.querySelector('.card');
+const url = 'https://api.adviceslip.com/advice';
+
+let cardId = card.querySelector('p').children[0];
+let cardAdvice = card.querySelector('h3');
+
+fetch(url,{
+    method: 'GET',
+})
+.then(response => response.json())
+.then(data => {
+    cardId.textContent = `#${data.slip.id}`;
+    cardAdvice.textContent = `"${data.slip.advice}"`;
+});
+
+dice.addEventListener('click',() => {
+    fetch(url,{
+        method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => {
+        cardId.textContent = "";
+        cardAdvice.textContent = "";
+        cardId.textContent = `#${data.slip.id}`;
+        cardAdvice.textContent = `"${data.slip.advice}"`;
+    })
+});
