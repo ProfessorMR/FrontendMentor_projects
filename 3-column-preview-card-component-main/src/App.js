552476b11b80ed302232b484c58a76927f88dadb
professor_mr <professormr29@gmail.com>
2023-08-04 15:04:10 +0330

    init 3 column preview card component project

---
+++ 3-column-preview-card-component-main/src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return <main className="App">asd</main>;
+};
+
+export default App;
---
+++ 3-column-preview-card-component-main/src/helpers/Colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+  BRIGHT_ORANGE: "hsl(31, 77%, 52%)",
+  DARK_CYAN: "hsl(184, 100%, 22%)",
+  VERY_DARK_CYAN: "hsl(179, 100%, 13%)",
+  TRANSPARENT_WHITE: "hsla(0, 0%, 100%, 0.75)",
+  VERY_LIGHT_GRAY: "hsl(0, 0%, 95%)",
+};
+
+export default Colors;
