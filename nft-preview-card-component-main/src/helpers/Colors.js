28a530808efa9e2770bfc7b882d61d0482b1f333
professor_mr <professormr29@gmail.com>
2023-08-06 14:40:01 +0330

    init nft preview card component project

---
+++ nft-preview-card-component-main/src/helpers/Colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+  SOFT_BLUE: "hsl(215, 51%, 70%)",
+  CYAN: "hsl(178, 100%, 50%)",
+  VERY_DARK_BLUE_MAIN: "hsl(217, 54%, 11%)",
+  VERY_DARK_BLUE_CARD: "hsl(216, 50%, 16%)",
+  VERY_DARK_BLUE_LINK: "hsl(215, 32%, 27%)",
+  WHITE: "hsl(0, 0%, 100%)",
+};
+
+export default Colors;
